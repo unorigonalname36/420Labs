b15072d77277e20a217938b95612d02f73f7f43f
unorigonalname36 <150210976+unorigonalname36@users.noreply.github.com>
2024-02-02 15:36:04 +0000

    Final working code without the test cases

--- Lab03.java
+++ Lab03.java
@@ -1,3 +1,38 @@
-public class Lab03
-    public static void main (String[] args{}){
+
+public class Lab03{
+    public static int cycleCount(int n){
+        int count=0;
+        while (n!=1){
+            if (n%2==0){
+                n=n/2;
+                count +=1;
+            }
+            else {
+                n=(n*3)+1;
+                count +=1;
+            }
+        }
+        return count;
     }
+    public static int algorithm(int i, int j){
+        int x = 0;
+        int y = 0;
+        System.out.print(i + " ");
+        System.out.print(j + " ");
+        while (i!=j){
+        x = cycleCount(i);
+        if (x>y){
+            y=x;
+        }
+        i+=1;
+        }
+
+
+        System.out.println(y+1);
+        return 0;
+    }
+    public static void main(String[] args) {
+        algorithm(900, 1000);
+
+    }
+}
